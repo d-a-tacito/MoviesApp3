bf31952016e098f04cdf25b7a36c15730cd3e9a0
d-a-tacito <dananto02@gmail.com>
2022-12-11 16:30:17 +0300

    Added MoviesApp with filters and other

---
+++ Filters/AgeOfActorAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AutoMapper;
+
+namespace MoviesApp.Filters;
+using System.ComponentModel.DataAnnotations;
+
+
+public class AgeOfActorAttribute : ValidationAttribute
+{
+    public AgeOfActorAttribute(int minAge,int maxAge)
+    {
+        MinAge = minAge;
+        MaxAge = maxAge;
+    }
+    public int MinAge { get; }
+    public int MaxAge { get; }
+
+    public string GetErrorMessage() =>
+        $"Actors must have birth date between {DateTime.Now.Year-MaxAge} and {DateTime.Now.Year - MinAge}.";
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        var birthYear = ((DateTime) value).Year;
+        Console.WriteLine("YearYearYear"+birthYear);
+        if (birthYear < DateTime.Now.Year - MaxAge || birthYear > DateTime.Now.Year - MinAge)
+        {
+            return new ValidationResult(GetErrorMessage());
+        }
+        return ValidationResult.Success;
+    }
+}
---
+++ Middleware/RequestLogMiddleware.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MoviesApp.Middleware
+{
+    public class RequestLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLogMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, ILogger<RequestLogMiddleware> logger)
+        {
+            logger.LogTrace($"Request: {httpContext.Request.Path}  Method: {httpContext.Request.Method}");
+            Console.WriteLine("-----------------"+httpContext.Request.Path+"---------"+httpContext.Request.Method);
+            await _next(httpContext);
+        }
+    }
+}
